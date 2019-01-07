9fb08ec175f9919e4baf48df667a71370b57fd26
Mihai C <mihai@wolfpack-digital.com>
2019-01-07 12:56:03 +0200

    Create registration/login/change password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+    end
+  end
 end
