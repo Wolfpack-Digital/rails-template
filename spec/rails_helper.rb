70a44c825c19b2cd88b2441d8504db069186c4ba
Florin Ionce <ionce.florin@gmail.com>
2020-07-22 18:11:33 +0300

    Add RequestSpecHelper and ApipieSpecHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require 'support/request_spec_helper'
+require 'support/apipie_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
@@ -15,6 +18,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include RequestSpecHelper, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/template.rb
+++ spec/template.rb
@@ -3,4 +3,5 @@ copy_file 'spec/spec_helper.rb'
 
 empty_directory_with_keep_file 'spec/mailers'
 empty_directory_with_keep_file 'spec/requests'
-empty_directory_with_keep_file 'spec/support'
+
+apply 'spec/support/template.rb'
