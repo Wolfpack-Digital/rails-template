39e9ba8b2a9887fa68f8ce8508e02cebb668b359
Robi Kudor <robi.kudor@yahoo.com>
2022-03-10 11:10:57 +0200

    Update app name in apipie initializer

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Apipie.configure do |config|
+  config.app_name                = 'AppName'
   config.api_base_url            = '/api'
   config.doc_base_url            = '/apipie'
   config.translate               = false
