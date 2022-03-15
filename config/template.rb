c8a9cf1f10e77cf31818ad6ffc697b95ceb32f5b
Robi Kudor <robi.kudor@yahoo.com>
2022-03-15 15:08:56 +0200

    Readd apipie AppName change

--- config/template.rb
+++ config/template.rb
@@ -9,6 +9,9 @@ copy_file 'config/initializers/generators.rb'
 copy_file 'config/initializers/rotate_log.rb'
 copy_file 'config/initializers/version.rb'
 copy_file 'config/initializers/apipie.rb'
+gsub_file 'config/initializers/apipie.rb', /AppName/ do
+  app_name.titleize
+end
 template 'config/initializers/sidekiq.rb.tt'
 
 gsub_file 'config/initializers/filter_parameter_logging.rb', /\[:password\]/ do
