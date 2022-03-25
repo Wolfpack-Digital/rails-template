ba9740dc3e9f679a61da0eddf4023e6a7ac9f37e
Siklodi Mariusz <9211589+siklodi-mariusz@users.noreply.github.com>
2022-03-25 11:21:14 +0200

    Auto require all spec support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'support/request_spec_helper'
-require 'support/apipie_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
